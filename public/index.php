7db2b6e858d083d36c313de9a23c6a815404ee35
Egor Rasskazov <rasskazov.note@gmail.com>
2022-08-02 19:01:04 +0300

    comm 2

--- public/index.php
+++ public/index.php
@@ -4,14 +4,39 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app = AppFactory::createFromContainer($container);
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = array_filter($users, fn ($user) => str_contains($user, $term));
+    $params = ['users' => $filteredUsers, 'term' => $term];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('a eto POST /users');
+});
+
+
+$app->get('/users/{id}', function ($request, $response, array $args) {
+    $params = ['id' => htmlspecialchars($args['id']), 'nickname' => 'user-' . ($args['id'])];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
+
 $app->run();
+
